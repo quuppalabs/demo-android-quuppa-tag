aee65c61335069845b1f68b9b2d1573a6374a30c
Kalle Korhonen <kalle.korhonen@quuppa.com>
2025-07-23 15:49:57 +0300

    Create a new pulsing icon button with animated radio waves

--- src/main/java/com/quuppa/quuppatag/PulsingQView.java
+++ src/main/java/com/quuppa/quuppatag/PulsingQView.java
@@ -10,48 +10,33 @@
  */
 package com.quuppa.quuppatag;
 
-import android.animation.ObjectAnimator;
 import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
 import android.util.AttributeSet;
-import android.animation.ArgbEvaluator;
+import androidx.appcompat.widget.AppCompatImageView;
 
-/**
- * Custom view to render Quuppa Q character on it with a pulsing animation.
- */
-public class PulsingQView extends androidx.appcompat.widget.AppCompatImageView {
+public class PulsingQView extends AppCompatImageView {
     private boolean pulsing = false;
-    private ObjectAnimator animator;
+    private AnimationDrawable waveAnimation;
 
     public PulsingQView(Context context, AttributeSet attrs) {
         super(context, attrs);
         setContentDescription("Quuppa Tag Service Status");
-
-        // Animating alpha on VectorDrawable simply doesn't seem to work, have to do with tint
-//        float[] values = new float[18];
-//        int index = 0;
-//        for (float f = 0.95f; f >= 0.10f; f -= 0.05f) values[index++] = f;
-
-        int steps = 15; // till ~30%
-        int[] tintColors = new int[steps + 1];
-        for (int i = 0; i <= steps; i++) {
-            int gray = 255 - (i * 13);
-            tintColors[i] = (255 << 24) | (gray << 16) | (gray << 8) | gray;
-        }
-
-        animator = ObjectAnimator.ofInt(getDrawable(), "tint", tintColors);
-        // Must use this evaluator, otherwise setTint interpolates to the existing color
-        animator.setEvaluator(new ArgbEvaluator());
-        animator.setDuration(1000);
-        animator.setRepeatCount(ObjectAnimator.INFINITE);
-        animator.setRepeatMode(ObjectAnimator.REVERSE);
+        setImageResource(R.drawable.q_white);
     }
 
     public void setIsPulsing(boolean isPulsing) {
         this.pulsing = isPulsing;
-        if (pulsing) animator.start();
-        else animator.cancel();
-        getDrawable().setTint(0xffffffff);
-        this.invalidate();
+        if (pulsing) {
+            setImageResource(R.drawable.wave_animation);
+            waveAnimation = (AnimationDrawable) getDrawable();
+            waveAnimation.start();
+        } else {
+            if (waveAnimation != null) {
+                waveAnimation.stop();
+            }
+            setImageResource(R.drawable.q_white);
+        }
         setContentDescription("Quuppa Tag Service " + (pulsing ? "enabled" : "disabled"));
     }
 }
